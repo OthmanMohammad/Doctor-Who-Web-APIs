a8f5d12a1979450cf5165407f0d8c693588e92ff
Mohammad Othman <emranm226@hotmail.com>
2023-04-05 11:12:26 +0100

    registered validators and added FluentValidation configuration

---
+++ DoctorWho.web/Validators/EnemyDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using DoctorWho.web.DTOs;
+
+namespace DoctorWho.Web.Validators
+{
+    public class EnemyDtoValidator : AbstractValidator<EnemyDto>
+    {
+        public EnemyDtoValidator()
+        {
+            RuleFor(x => x.EnemyName).NotEmpty().WithMessage("Enemy name is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+        }
+    }
+}
---
+++ DoctorWho.web/Validators/EpisodeDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using DoctorWho.web.DTOs;
+
+namespace DoctorWho.Web.Validators
+{
+    public class EpisodeDtoValidator : AbstractValidator<EpisodeDto>
+    {
+        public EpisodeDtoValidator()
+        {
+            RuleFor(x => x.SeriesNumber)
+                .Must(x => x.HasValue && x.ToString().Length == 10)
+                .WithMessage("Series number must be 10 characters long.");
+
+            RuleFor(x => x.EpisodeNumber).GreaterThan(0).WithMessage("Episode number must be greater than 0.");
+            RuleFor(x => x.AuthorId).NotEmpty().WithMessage("Author ID is required.");
+            RuleFor(x => x.DoctorId).NotEmpty().WithMessage("Doctor ID is required.");
+        }
+    }
+}
