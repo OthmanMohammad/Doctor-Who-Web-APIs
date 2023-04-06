1d4f5f7721790bf4f37bb95c69f06fbc49dfc3d0
Mohammad Othman <emranm226@hotmail.com>
2023-04-06 08:14:49 +0100

    Created a custom exception middleware to handle exceptions

---
+++ DoctorWho.web/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace DoctorWho.Web.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+
+            // You can add more specific exception handling here, e.g.:
+            // if (exception is NotFoundException) statusCode = HttpStatusCode.NotFound;
+
+            string result = JsonSerializer.Serialize(new { error = exception.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
--- DoctorWho.web/Program.cs
+++ DoctorWho.web/Program.cs
@@ -1,6 +1,29 @@
-﻿namespace DoctorWho.web
+﻿using DoctorWho.Web.Validators;
+using DoctorWho.Web.Middleware;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add controllers and FluentValidation
+builder.Services.AddControllers()
+        .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<EpisodeDtoValidator>());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
