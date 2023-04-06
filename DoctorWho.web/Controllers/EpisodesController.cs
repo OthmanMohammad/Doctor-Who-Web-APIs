e026e8b26fbbd95cb83301cc6d9cf73e2fd5de5f
Mohammad Othman <emranm226@hotmail.com>
2023-04-06 09:25:06 +0100

    Added a create,update,delete,getEpisodebyID,getAllEpisodes API for episodes

--- DoctorWho.web/Controllers/EpisodesController.cs
+++ DoctorWho.web/Controllers/EpisodesController.cs
@@ -1,12 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoctorWho.Db.Repositories;
+using DoctorWho.Domain;
+using DoctorWho.web.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace DoctorWho.web.Controllers
 {
-    public class EpisodesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EpisodesController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IEpisodesRepository _episodesRepository;
+
+        public EpisodesController(IEpisodesRepository episodesRepository)
         {
-            return View();
+            _episodesRepository = episodesRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateEpisode([FromBody] EpisodeDto episodeDto)
+        {
+            _episodesRepository.Create(episodeDto.SeriesNumber, episodeDto.EpisodeNumber, episodeDto.Title, episodeDto.EpisodeDate, episodeDto.AuthorId, episodeDto.DoctorId, episodeDto.Notes);
+            return Ok("Episode created successfully");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEpisode([FromBody] EpisodeDto episodeDto)
+        {
+            var episode = new Episode
+            {
+                EpisodeId = episodeDto.EpisodeId,
+                SeriesNumber = episodeDto.SeriesNumber,
+                EpisodeNumber = episodeDto.EpisodeNumber,
+                Title = episodeDto.Title,
+                EpisodeDate = episodeDto.EpisodeDate,
+                AuthorId = episodeDto.AuthorId,
+                DoctorId = episodeDto.DoctorId,
+                Notes = episodeDto.Notes
+            };
+
+            _episodesRepository.Update(episode);
+            return Ok("Episode updated successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEpisode(int id)
+        {
+            Episode episode = _episodesRepository.GetById(id);
+            if (episode == null) return NotFound("Episode not found");
+
+            _episodesRepository.Delete(episode);
+            return Ok("Episode deleted successfully");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEpisodeById(int id)
+        {
+            Episode episode = _episodesRepository.GetById(id);
+            if (episode == null) return NotFound("Episode not found");
+
+            return Ok(episode);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEpisodes()
+        {
+            var episodes = _episodesRepository.GetAll();
+            return Ok(episodes);
         }
     }
 }
