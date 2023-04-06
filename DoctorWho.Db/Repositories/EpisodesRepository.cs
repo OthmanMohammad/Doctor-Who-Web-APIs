1d50f274c558d87844cc62ca9c0845d5ddba302c
Mohammad Othman <emranm226@hotmail.com>
2023-04-06 12:23:14 +0100

    Implemented Upsert API for doctors and referencing incorrect files

--- DoctorWho.Db/Repositories/EpisodesRepository.cs
+++ DoctorWho.Db/Repositories/EpisodesRepository.cs
@@ -1,33 +1,59 @@
 ﻿using DoctorWho.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DoctorWho.Db.Repositories
 {
-    public class EpisodesRepository
+    public class EpisodesRepository : IEpisodesRepository
     {
-        public static void Create(int? SeriesNumber, int? EpisodeNumber, string EpisodeType, string Title, DateTime? EpisodeDate, int AuthorId, DoctorIdEnum DoctorId, string Notes)
+        private readonly DoctorWhoCoreDbContext _context;
+
+        public EpisodesRepository(DoctorWhoCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(int? SeriesNumber, int? EpisodeNumber, string Title, DateTime? EpisodeDate, int AuthorId, DoctorIdEnum DoctorId, string Notes)
         {
             if (Title == null) throw new ArgumentNullException("Cannot create an Episode with a null Title!");
-            DoctorWhoCoreDbContext._context.Episodes.Add(new Episode(SeriesNumber, EpisodeNumber, EpisodeType, Title, EpisodeDate, AuthorId, DoctorId, Notes));
-            DoctorWhoCoreDbContext._context.SaveChanges();
+            _context.Episodes.Add(new Episode(SeriesNumber, EpisodeNumber, null, Title, EpisodeDate, AuthorId, DoctorId, Notes));
+            _context.SaveChanges();
         }
-        public static void Update()
+
+        public void Update(Episode episode)
         {
-            DoctorWhoCoreDbContext._context.ChangeTracker.DetectChanges();
-            DoctorWhoCoreDbContext._context.SaveChanges();
+            var existingEpisode = GetById(episode.EpisodeId);
+            if (existingEpisode == null) throw new ArgumentException("Episode not found");
+
+            existingEpisode.SeriesNumber = episode.SeriesNumber;
+            existingEpisode.EpisodeNumber = episode.EpisodeNumber;
+            existingEpisode.EpisodeType = episode.EpisodeType;
+            existingEpisode.Title = episode.Title;
+            existingEpisode.EpisodeDate = episode.EpisodeDate;
+            existingEpisode.AuthorId = episode.AuthorId;
+            existingEpisode.DoctorId = episode.DoctorId;
+            existingEpisode.Notes = episode.Notes;
+
+            _context.SaveChanges();
         }
-        public static void Delete(Episode Episode)
+
+        public void Delete(Episode episode)
         {
-            if (Episode == null) throw new ArgumentNullException("Cannot remove a null Episode from the Episodes table");
-            try
-            {
-                DoctorWhoCoreDbContext._context.Episodes.Remove(Episode);
-                DoctorWhoCoreDbContext._context.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            if (episode == null) throw new ArgumentNullException(nameof(episode));
+            _context.Episodes.Remove(episode);
+            _context.SaveChanges();
+        }
+
+        public Episode GetById(int id)
+        {
+            return _context.Episodes.Find(id);
+        }
+
+        public IEnumerable<Episode> GetAll()
+        {
+            return _context.Episodes.ToList();
         }
     }
 }
---
+++ DoctorWho.Db/Repositories/IEpisodesRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorWho.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace DoctorWho.Db.Repositories
+{
+    public interface IEpisodesRepository
+    {
+        void Create(int? SeriesNumber, int? EpisodeNumber, string Title, DateTime? EpisodeDate, int AuthorId, DoctorIdEnum DoctorId, string Notes);
+        void Update(Episode episode);
+        void Delete(Episode Episode);
+        Episode GetById(int id);
+        IEnumerable<Episode> GetAll();
+    }
+}
